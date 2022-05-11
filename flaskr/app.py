3cf9267cb59dd2fecd0aee53284371e42ef4701e
mteam88 <matthew8edelen@gmail.com>
2022-05-11 23:30:58 +0000

    static renders

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,11 +2,14 @@ from flask import Flask
 import flask
 
 
-app = Flask(__name__, template_folder='flaskr/templates')
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static/',
+            template_folder='templates/')
 
 @app.route('/')
-def home():
-    return flask.render_template('views/home.html')
+def homehandler():
+    return flask.render_template('views/home.html', stockwidgets=['AAPL', 'TSLA', 'SPY'])
 
 if __name__ == '__main__':
     app.debug = True
