2c38433471f15e2e42d98851396aa04472f78ded
mteam88 <matthew8edelen@gmail.com>
2022-05-11 19:20:52 +0000

    various

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import flask
 
-app = Flask("main")
+
+app = Flask(__name__, template_folder='flaskr/templates')
 
 @app.route('/')
-
+def home():
+    return flask.render_template('views/home.html')
 
 if __name__ == '__main__':
     app.debug = True
