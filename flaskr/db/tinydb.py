d7c93fdf6f77cb612b595bb0e07cf59dbe4a7b2b
mteam88 <matthew8edelen@gmail.com>
2022-05-15 19:21:39 +0000

    add is_user and refactored hashing algo

--- flaskr/db/tinydb.py
+++ flaskr/db/tinydb.py
@@ -3,10 +3,16 @@ import bcrypt
 
 accountdb = TinyDB('accountdb.json')
 
-def register_user(registrationform):
-    salt = bcrypt.gensalt()
-    hashedpasswd = str(bcrypt.hashpw(bytes(registrationform.username.data, encoding='utf-8'), salt))
+UserQuery=Query()
 
+def register_user(registrationform):
+    hashedpasswd = hashsaltpassword(registrationform.password.data)
     accountdb.insert({'username': registrationform.username.data, 'password': hashedpasswd})
 
-def is_user()
+def is_user(loginform):
+    'returns True if login form correlates to a user else False'
+    return (accountdb.search((UserQuery.username == loginform.username.data) & (UserQuery.password == hashsaltpassword(loginform.password.data))))
+
+def hashsaltpassword(password: str):
+    salt = bcrypt.gensalt()
+    return str(bcrypt.hashpw(bytes(registrationform.username.data, encoding='utf-8'), salt))
